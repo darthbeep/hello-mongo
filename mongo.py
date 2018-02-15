3280aa29419fb98ace4bdbb127743957ed07d8eb
Yuyang Zhang <yzhang4@stuy.edu>
2018-02-15 06:04:46 -0500

    Finished point 4,5

--- mongo.py
+++ mongo.py
@@ -15,7 +15,31 @@ def find_zip(z):
 def find_zip_grade(z, g):
     return collection.find({'address.zipcode':z, 'grades.0.grade':g})
 
+for thing in find_borough("Brooklyn"):
+    print thing
+
+for thing in find_zip("11215"):
+    print thing
+
 for thing in find_zip_grade('11215', 'A'):
     print thing
 #for thing in db:
-#    print thing
+#   print thing
+
+
+def find_zip_grade_lt(z,score):
+    return collection.find({"$and": [{"address.zipcode":z},{"grades.0.score":{"$lt": score }}]})
+
+for thing in find_zip_grade_lt('11215', 1):
+    print thing
+
+
+
+def special(borough, score, grade):
+    return collection.find({'$and': [{"borough": borough}, {"grades.0.score": score}, {"grades.0.grade": grade}]})
+
+for thing in special("Brooklyn", 6, "A"):
+    print thing
+                         
+ 
+
